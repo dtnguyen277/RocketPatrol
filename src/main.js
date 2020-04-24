c79dab4a96a252e61b7ec0c4fa411a06980cf9b4
dtnguyen277 <dtnguyen277@users.noreply.github.com>
2020-04-23 21:57:22 -0700

    finished

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ let config = {
     height: 480,
     scene: [ Menu, Play ],
 };
+
 let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+
+// reserve some keyboard variables
+let keyF, keyLEFT, keyRIGHT;
