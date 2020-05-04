fa291b6dd913ff1d8bc2295640191dfd0dfd9f43
dtnguyen277 <dtnguyen277@users.noreply.github.com>
2020-05-04 00:09:39 -0700

    DONE2

--- src/main.js
+++ src/main.js
@@ -10,6 +10,22 @@
 // Randomize each spaceship's movement direction at the start of each play (10)
 // Create a new scrolling tile sprite for the background (10)
 // Allow the player to control the Rocket after it's fired (10)
+/*
+While programming this game I used new methods that I learned a lot and used new things that 
+I had never done before. An example of this is the procedural obstacle generation I created.
+Instead of respawning and recylcing obstacles. I used a stack where I pushed on new obstacles
+spliced off old ones when they weren't needed. I also created a difficulty curve where the 
+spawn frequency and speed of the road increased as you dodged more obstacles and slowed down
+when you ran into obstacles.
+We learned a lot about video game asset creation and what goes into creating the visuals of a game. 
+Before this, Rakhi had only made a handful of pixel art in her spare time but never did anything with 
+fully top-down perspectives. She is especially proud of her explosion animation when the bus hits the 
+police cars. We tried making the endless runner timed by the amount of gas in the bus, allowing the 
+player to prolong their game by collecting gas on the road. However the as the game speeds up, it 
+becomes harder and harder to collect the gas while making sure their score doesn't go down by avoiding 
+obstacles. We also gave the player the ability to avoid the end of their run by allowing them to hit 
+the police cars that would otherwise cause them to lose the game if they slow down far enough.
+*/
 
 let config = {
     type: Phaser.CANVAS,
